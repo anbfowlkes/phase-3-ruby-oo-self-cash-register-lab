c04517970f27ba224be0c6a09e2bb52ef707d548
Aaron Fowlkes <anbfowlkes@gmail.com>
2022-08-13 15:31:21 -0400

    discount function

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+class CashRegister
+    attr_accessor :discount, :total
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+    end
 
+    def total
+        @total
+    end
+    def total=(num)
+        @total = num
+    end
+
+    def add_item(title, price, quantity = 1)
+        @total += price * quantity
+    end
+
+    def apply_discount
+        self.total = self.total * (1 - self.discount/100.0)
+    end
+
+end
